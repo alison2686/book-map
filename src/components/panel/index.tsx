da74e69913930b64f5184bec2e48bfb9298c9e6f
Alison Lee <alison2686@gmail.com>
2024-05-22 18:28:51 -0700

    panel grid col-span

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -6,23 +6,23 @@ function ComicPanel() {
     <div className='container mx-auto'>
       <div className='flex items-center justify-center min-h-screen p-4'>
         <div className='border-8 border-black max-w-6xl bg-comic-background bg-cover w-full h-screen flex items-center justify-center'>
-          <div className='grid grid-cols-3 grid-rows-2 gap-4 w-3/4 h-3/4 mt-auto mb-10'>
+          <div className='grid grid-cols-6 grid-rows-2 gap-4 w-3/4 h-3/4 mt-auto mb-8'>
             {/* Top row: 3 square panels */}
-            <div className='bg-gray-200 border-2 border-black flex items-center justify-center'>
+            <div className='col-span-2 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
               Panel 1
             </div>
-            <div className='bg-gray-200 border-2 border-black flex items-center justify-center'>
+            <div className='col-span-2 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
               Panel 2
             </div>
-            <div className='bg-gray-200 border-2 border-black flex items-center justify-center'>
+            <div className='col-span-2 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
               Panel 3
             </div>
 
             {/* Bottom row: 2 rectangular panels */}
-            <div className='col-span-2 bg-gray-200 border-2 border-black flex items-center justify-center'>
+            <div className='col-span-3 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
               Panel 4
             </div>
-            <div className='bg-gray-200 border-2 border-black flex items-center justify-center'>
+            <div className='col-span-3 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
               Panel 5
             </div>
           </div>
