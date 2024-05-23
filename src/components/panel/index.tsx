1aa3cb52cfe5df6a76623d0da6ce23a4ec8db1c1
Alison Lee <alison2686@gmail.com>
2024-05-23 12:51:06 -0700

    offset panel 4

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -10,25 +10,25 @@ function ComicPanel() {
             <h1 className='text-6xl col-span-full row-span-2 flex items-center justify-center rounded'>
               Panel Title
             </h1>
-            <div className='col-span-full border-4 border-black flex items-center justify-center rounded'>
+            <div className='col-span-full flex items-center justify-center rounded'>
               Panel Subtitle
             </div>
             {/* Top row: 3 square panels */}
-            <div className='col-span-2 row-span-4 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
+            <div className='col-span-2 row-span-4 bg-slate-50 border-4 border-black shadow-2xl flex items-center justify-center rounded'>
               Panel 1
             </div>
-            <div className='col-span-2 row-span-4 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
+            <div className='col-span-2 row-span-4 bg-slate-50 border-4 border-black shadow-2xl flex items-center justify-center rounded'>
               Panel 2
             </div>
-            <div className='col-span-2 row-span-4 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
+            <div className='col-span-2 row-span-4 bg-slate-50 border-4 border-black shadow-2xl flex items-center justify-center rounded'>
               Panel 3
             </div>
 
             {/* Bottom row: 2 rectangular panels */}
-            <div className='col-span-3 row-span-4 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
+            <div className='col-span-3 row-span-4 bg-slate-50 border-4 border-black shadow-2xl flex items-center justify-center rounded transform translate-y-2'>
               Panel 4
             </div>
-            <div className='col-span-3 row-span-4 bg-slate-50 border-4 border-black flex items-center justify-center rounded'>
+            <div className='col-span-3 row-span-4 bg-slate-50 border-4 border-black shadow-2xl flex items-center justify-center rounded'>
               Panel 5
             </div>
           </div>
