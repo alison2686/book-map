8b780902c8f1ee021b4e8a127806efad78fd6f30
Alison Lee <alison2686@gmail.com>
2024-06-13 15:06:32 -0700

    Little Pete comic panel data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Hero from '@/components/hero';
 import Map from '@/components/map';
-import ComicPanel from '@/components/panel';
-// import ComicPage from '@/components/strip';
 import Comic from '@/components/comic';
 
 export default function Home() {
@@ -9,9 +7,7 @@ export default function Home() {
     <>
       <Hero />
       <Map />
-      {/* <ComicPage /> */}
       <Comic />
-      {/* <ComicPanel /> */}
     </>
   );
 }
--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -5,31 +5,49 @@ import { FC } from 'react';
 
 interface ComicPanelProps {
   title: string;
+  subtitle: string;
+  img1: string;
+  alt1: string;
+  img2: string;
+  alt2: string;
+  img3: string;
+  alt3: string;
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
 }
 
-const ComicPanel: FC<ComicPanelProps> = ({ title }) => {
+const ComicPanel: FC<ComicPanelProps> = ({
+  title,
+  subtitle,
+  img1,
+  alt1,
+  img2,
+  alt2,
+  img3,
+  alt3,
+  text1,
+  text2,
+  text3,
+  text4,
+}) => {
   return (
     <div className='container mx-auto p-4 '>
       <div className='flex items-center justify-center'>
         <div className='border-4 border-black bg-comic-background bg-cover w-full h-screen flex items-center justify-center'>
           <div className='grid grid-cols-6 grid-rows-10 gap-4 w-full h-full p-6'>
-            <h1
-              className='text-8xl col-span-full row-span-2 flex items-center justify-center [text-shadow:_6px_3px_0_rgb(255_255_255_/_90%)]'
-              title={title}
-            >
+            <h1 className='text-8xl col-span-full row-span-2 flex items-center justify-center [text-shadow:_6px_3px_0_rgb(255_255_255_/_90%)]'>
               {title}
             </h1>
             <div className='col-span-full flex items-center justify-center'>
-              During the 1895 San Francisco Fair at Golden Gate Park, hooligan
-              Fong Ching (Little Pete), a city-slicker, police dupe, and stool
-              pigeon, became involved with international kidnapping of Chinese
-              girls from Hong Kong and China to dance at the Parks events
+              {subtitle}
             </div>
             {/* Top row: 3 square panels */}
             <div className='col-span-2 row-span-4 comic-panel'>
               <Image
-                src='/images/little-pete1.png'
-                alt='barber'
+                src={img1}
+                alt={alt1}
                 width={500}
                 height={500}
                 className='w-full h-full object-cover'
@@ -37,24 +55,22 @@ const ComicPanel: FC<ComicPanelProps> = ({ title }) => {
             </div>
             <div className='col-span-2 row-span-4 comic-panel relative'>
               <Image
-                src='/images/ggp.png'
-                alt='barber'
+                src={img2}
+                alt={alt2}
                 width={500}
                 height={500}
                 className='w-full h-full object-cover'
               />
               <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
                 <div className='absolute bottom-0 left-0 text-white text-center p-4 bg-black bg-opacity-70'>
-                  The Arizona Suey Ying Tong men wanted to use the assassination
-                  of Little Pete as an unspoken warning to San Francisco rogue
-                  police.
+                  {text1}
                 </div>
               </div>
             </div>
             <div className='col-span-2 row-span-4 comic-panel relative'>
               <Image
-                src='/images/barber.png'
-                alt='barber'
+                src={img3}
+                alt={alt3}
                 width={500}
                 height={500}
                 className='w-full h-full object-cover'
@@ -62,26 +78,16 @@ const ComicPanel: FC<ComicPanelProps> = ({ title }) => {
 
               <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
                 <div className='absolute bottom-0 left-0 text-white text-center p-4 bg-black bg-opacity-70'>
-                  Before the assassination, San Franciscos Yellow Journalism
-                  news media were forwarned about the impending murder, thus the
-                  newspaper reporters were at the mirder scene minutes after the
-                  killing.
+                  {text2}
                 </div>
               </div>
             </div>
             {/* Bottom row: 2 rectangular panels */}
             <div className='col-span-3 row-span-4 comic-panel transform translate-y-2 p-10'>
-              Suey Ying Tong elders told the story that the Tombstone AZ killers
-              chose January 23, 1897, during the Chinese New Year celebration,
-              when there were a lot of out-of-towners and strangers in the City
-              as the right time to assasinate Little Pete and leave town
-              unnoticed.
+              {text3}
             </div>
             <div className='col-span-3 row-span-4 comic-panel p-10'>
-              Albert Bew Chan (Lucretias grandfather) said that he was the ten
-              year old messanger boy who deliverd the killers message via their
-              Chinatown telephone switchboard to Tombstone that Little Pete had
-              been killed.
+              {text4}
             </div>
           </div>
         </div>
