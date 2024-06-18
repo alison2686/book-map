6e05f4548548ddd5759b601240e5d6aa8f81c4ad
Alison Lee <alison2686@gmail.com>
2024-06-18 16:50:03 -0700

    add map icons

--- src/components/map/CathcartMap.tsx
+++ src/components/map/CathcartMap.tsx
@@ -2,7 +2,16 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSkull, faSailboat } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSkull,
+  faSailboat,
+  faSquarePhone,
+  faShrimp,
+  faVihara,
+  faJoint,
+  faShirt,
+  faHotel,
+} from '@fortawesome/free-solid-svg-icons';
 
 const CathcartMap = () => {
   // Update the size of CathcartMap when the size of the screen changes
@@ -37,14 +46,50 @@ const CathcartMap = () => {
             {/* Placing the icon on the image */}
             <FontAwesomeIcon
               icon={faSkull}
-              className='absolute text-blue-500'
+              className='absolute text-black'
               style={{ top: '40%', left: '75%', fontSize: '2rem' }}
               onClick={() => alert('Icon Clicked')}
             />
             <FontAwesomeIcon
               icon={faSailboat}
-              className='absolute text-red-700'
-              style={{ top: '25%', left: '5%', fontSize: '2rem' }}
+              className='absolute text-black'
+              style={{ top: '85%', left: '71%', fontSize: '2rem' }}
+              onClick={() => alert('Icon Clicked')}
+            />
+            <FontAwesomeIcon
+              icon={faSquarePhone}
+              className='absolute text-black'
+              style={{ top: '76%', left: '77%', fontSize: '2rem' }}
+              onClick={() => alert('Icon Clicked')}
+            />
+            <FontAwesomeIcon
+              icon={faShrimp}
+              className='absolute text-black'
+              style={{ top: '20%', left: '1%', fontSize: '2rem' }}
+              onClick={() => alert('Icon Clicked')}
+            />
+            <FontAwesomeIcon
+              icon={faVihara}
+              className='absolute text-black'
+              style={{ top: '72%', left: '59%', fontSize: '2rem' }}
+              onClick={() => alert('Icon Clicked')}
+            />
+            <FontAwesomeIcon
+              icon={faJoint}
+              className='absolute text-black'
+              style={{ top: '93%', left: '50%', fontSize: '2rem' }}
+              onClick={() => alert('Icon Clicked')}
+            />
+            <FontAwesomeIcon
+              icon={faShirt}
+              className='absolute text-black'
+              style={{ top: '62%', left: '75%', fontSize: '2rem' }}
+              onClick={() => alert('Icon Clicked')}
+            />
+            <FontAwesomeIcon
+              icon={faHotel}
+              className='absolute text-black'
+              style={{ top: '72%', left: '65%', fontSize: '2rem' }}
               onClick={() => alert('Icon Clicked')}
             />
           </div>
