4fb52ab333df2cfae46383fad2a3982795d53cb0
Alison Lee <alison2686@gmail.com>
2024-07-11 14:35:49 -0700

    fix popup positioning

--- src/components/map/CathcartMap.tsx
+++ src/components/map/CathcartMap.tsx
@@ -48,8 +48,8 @@ const CathcartMap = () => {
       .closest('.relative-container')!
       .getBoundingClientRect();
 
-    const popupTop = iconRect.top - containerRect.top + iconRect.height - 180;
-    const popupLeft = iconRect.left - containerRect.left + iconRect.width - 290;
+    const popupTop = iconRect.top - containerRect.top + iconRect.height / 2; // Adjust top position to align better
+    const popupLeft = iconRect.left - containerRect.left + iconRect.width + 10; // Adding 10px for some spacing
 
     setPopup({
       visible: true,
@@ -58,6 +58,7 @@ const CathcartMap = () => {
       left: popupLeft,
     });
   };
+
   return (
     <div className='container mx-auto px-4'>
       <div className='relative-container w-full h-screen flex justify-center items-center bg-sky overflow-hidden relative'>
@@ -65,75 +66,124 @@ const CathcartMap = () => {
           <h1 className='text-4xl lg:text-6xl font-bold text-shadow-lg p-6'>
             Cathcart Map <br /> of San Francisco’s Chinatown
           </h1>
-          <div className='relative border-4 border-black'>
+          <div className='relative border-4 border-black relative-container'>
             <Image
               src='/images/map.jpeg'
               alt='Cathcart Map'
               width={width < 1024 ? 300 : 800}
               height={width < 1024 ? 400 : 800}
             />
-            <FontAwesomeIcon
-              icon={faSkull}
-              className='map-icon'
-              style={{ top: '40%', left: '75%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('Little Pete Must Die', e)}
-            />
-            <FontAwesomeIcon
-              icon={faSailboat}
-              className='map-icon'
-              style={{ top: '85%', left: '71%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('Robert Louis Stevenson', e)}
-            />
-            <FontAwesomeIcon
-              icon={faSquarePhone}
-              className='map-icon'
-              style={{ top: '76%', left: '77%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('Chinese Telephone Exchange', e)}
-            />
-            <FontAwesomeIcon
-              icon={faShrimp}
-              className='map-icon'
-              style={{ top: '20%', left: '1%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('The Mhystery of China Beach', e)}
-            />
-            <FontAwesomeIcon
-              icon={faVihara}
-              className='map-icon'
-              style={{ top: '72%', left: '59%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('Shing Chong', e)}
-            />
-            <FontAwesomeIcon
-              icon={faJoint}
-              className='map-icon'
-              style={{ top: '90%', left: '50%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('Cigar', e)}
-            />
-            <FontAwesomeIcon
-              icon={faShirt}
-              className='map-icon'
-              style={{ top: '62%', left: '75%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('First Chinese Laundry', e)}
-            />
-            <FontAwesomeIcon
-              icon={faHotel}
-              className='map-icon'
-              style={{ top: '72%', left: '65%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('Suey ying Tong Building', e)}
-            />
-            <FontAwesomeIcon
-              icon={faHouseUser}
-              className='map-icon'
-              style={{ top: '22%', left: '35%', fontSize: '2rem' }}
-              onClick={(e) => handleIconClick('The Donaldina Cameron House', e)}
-            />
+            <div
+              className='icon-container absolute'
+              style={{ top: '40%', left: '75%' }}
+            >
+              <FontAwesomeIcon
+                icon={faSkull}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) => handleIconClick('Little Pete Must Die', e)}
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '85%', left: '71%' }}
+            >
+              <FontAwesomeIcon
+                icon={faSailboat}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) => handleIconClick('Robert Louis Stevenson', e)}
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '76%', left: '77%' }}
+            >
+              <FontAwesomeIcon
+                icon={faSquarePhone}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) =>
+                  handleIconClick('Chinese Telephone Exchange', e)
+                }
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '20%', left: '1%' }}
+            >
+              <FontAwesomeIcon
+                icon={faShrimp}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) =>
+                  handleIconClick('The Mystery of China Beach!!', e)
+                }
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '72%', left: '59%' }}
+            >
+              <FontAwesomeIcon
+                icon={faVihara}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) => handleIconClick('Shing Chong', e)}
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '90%', left: '50%' }}
+            >
+              <FontAwesomeIcon
+                icon={faJoint}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) => handleIconClick('Cigar', e)}
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '62%', left: '75%' }}
+            >
+              <FontAwesomeIcon
+                icon={faShirt}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) => handleIconClick('First Chinese Laundry', e)}
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '72%', left: '65%' }}
+            >
+              <FontAwesomeIcon
+                icon={faHotel}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) => handleIconClick('Suey ying Tong Building', e)}
+              />
+            </div>
+            <div
+              className='icon-container absolute'
+              style={{ top: '22%', left: '35%' }}
+            >
+              <FontAwesomeIcon
+                icon={faHouseUser}
+                className='map-icon'
+                style={{ fontSize: '2rem' }}
+                onClick={(e) =>
+                  handleIconClick('The Donaldina Cameron House', e)
+                }
+              />
+            </div>
             {popup.visible && (
               <div
                 className='absolute bg-white text-black border border-black p-2 rounded shadow-lg'
                 style={{
                   top: `${popup.top}px`,
                   left: `${popup.left}px`,
-                  zIndex: 10,
-                  transform: 'translate(-50%, 0)', // Center horizontally
                 }}
               >
                 {popup.content}
