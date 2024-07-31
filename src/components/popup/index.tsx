cf2692177eae4f5d67dc032971d505b80fcdff18
Alison Lee <alison2686@gmail.com>
2024-07-31 16:09:38 -0700

    popup width

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -13,21 +13,23 @@ const PopupCard: FC<PopupCardProps> = ({
   page,
 }) => {
   return (
-    <div className='card bg-white text-black border border-black p-4 rounded shadow-lg w-full max-w-5xl sm:max-w-lg md:max-w-xl lg:max-w-2xl relative'>
-      <button
-        className='absolute top-2 right-2 text-black'
-        onClick={onClose}
-        aria-label='Close'
-      >
-        <FontAwesomeIcon icon={faTimes} />
-      </button>
-      <p className='text-xl sm:text-2xl font-bold'>{title}</p>
-      <p className='text-md sm:text-lg'>{chapter}</p>
-      <p className='text-sm'>{page}</p>
-      <p className='text-sm sm:text-base'>{subtitle}</p>
-      <Link href={`/Comics/${id}`} className='text-blue-500 underline'>
-        Learn More...
-      </Link>
+    <div className='fixed inset-0 flex items-center justify-center p-4'>
+      <div className='card bg-white text-black border border-black p-4 rounded shadow-lg min-w-96 sm:max-w-lg md:max-w-xl lg:max-w-2xl relative'>
+        <button
+          className='absolute top-2 right-2 text-black'
+          onClick={onClose}
+          aria-label='Close'
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+        <p className='text-xl sm:text-2xl font-bold'>{title}</p>
+        <p className='text-md sm:text-lg'>{chapter}</p>
+        <p className='text-sm'>{page}</p>
+        <p className='text-sm sm:text-base'>{subtitle}</p>
+        <Link href={`/Comics/${id}`} className='text-blue-500 underline'>
+          Learn More...
+        </Link>
+      </div>
     </div>
   );
 };
