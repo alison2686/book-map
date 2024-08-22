6008b97c1b2245913e79101d2ffa4e6d6140b9cf
Alison Lee <alison2686@gmail.com>
2024-08-22 15:53:11 -0700

    move china beach icon

--- src/components/map/CathcartMap.tsx
+++ src/components/map/CathcartMap.tsx
@@ -30,7 +30,6 @@ type IconKey =
   | 'tong'
   | 'cameron';
 
-// Lazy load the FontAwesomeIcon component
 const FontAwesomeIcon = lazy(() =>
   import('@fortawesome/react-fontawesome').then((module) => ({
     default: module.FontAwesomeIcon,
@@ -47,7 +46,6 @@ const CathcartMap = () => {
   });
 
   useEffect(() => {
-    // Check if running in client-side
     if (typeof window !== 'undefined') {
       const updateWidth = () => {
         const newWidth = window.innerWidth;
@@ -77,7 +75,6 @@ const CathcartMap = () => {
     let popupLeft = iconRect.left - containerRect.left + iconRect.width + 50;
 
     if (width < 768) {
-      // Adjust position for mobile screens
       popupLeft = containerRect.width / 2;
     }
 
@@ -117,7 +114,7 @@ const CathcartMap = () => {
 
   return (
     <div className='container mx-auto px-4 overflow-visible'>
-      <div className='w-full h-full flex justify-center items-center bg-sky'>
+      <div className='w-full h-full flex flex-col items-center bg-sky'>
         <HomeButton />
         <div className='flex flex-col justify-center items-center text-center w-full'>
           <h1 className='text-4xl lg:text-6xl font-bold text-shadow-lg pt-20 mt-10'>
@@ -128,62 +125,69 @@ const CathcartMap = () => {
             Click on any icon on the map and a dialogue box will appear giving a
             little information about the place selected.
           </h3>
-          <div className='border-4 border-black relative-container overflow-visible m-2'>
-            {width > 0 && (
-              <Image
-                src='/images/map.jpeg'
-                alt='Cathcart Map'
-                width={width < 1024 ? 300 : 800}
-                height={width < 1024 ? 400 : 800}
-              />
-            )}
-            {Object.keys(icons).map((key) => (
-              <div
-                className='icon-container'
-                style={getIconPositionStyle(key as IconKey)}
-                key={key}
-              >
-                <Suspense fallback={<div>Loading...</div>}>
-                  <div
-                    onClick={(e) => handleIconClick(key, e)}
-                    className='map-icon'
-                  >
-                    <FontAwesomeIcon icon={icons[key as IconKey] as IconProp} />
-                  </div>
-                </Suspense>
-              </div>
-            ))}
-            {popup.visible && popup.content && (
-              <div
-                className={`absolute z-50 ${
-                  width < 768
-                    ? 'fixed inset-0 flex items-center justify-center'
-                    : ''
-                }`}
-                style={{
-                  top: `${popup.top}px`,
-                  left: `${popup.left}px`,
-                  transform: width < 768 ? 'none' : 'translate(-50%, -50%)',
-                }}
-              >
-                <PopupCard
-                  id={popup.content.id}
-                  title={popup.content.title}
-                  chapter={popup.content.chapter}
-                  page={popup.content.page}
-                  subtitle={popup.content.subtitle}
-                  onClose={handleClose}
+
+          {/* First row with map.jpeg */}
+          <div className='flex justify-center items-center'>
+            <div className='border-4 border-black relative-container overflow-visible m-8'>
+              {width > 0 && (
+                <Image
+                  src='/images/map.jpeg'
+                  alt='Cathcart Map'
+                  width={width < 1024 ? 300 : 800}
+                  height={width < 1024 ? 400 : 800}
                 />
-              </div>
-            )}
+              )}
+              {Object.keys(icons).map((key) => (
+                <div
+                  className='icon-container'
+                  style={getIconPositionStyle(key as IconKey)}
+                  key={key}
+                >
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <div
+                      onClick={(e) => handleIconClick(key, e)}
+                      className='map-icon'
+                    >
+                      <FontAwesomeIcon
+                        icon={icons[key as IconKey] as IconProp}
+                      />
+                    </div>
+                  </Suspense>
+                </div>
+              ))}
+              {popup.visible && popup.content && (
+                <div
+                  className={`absolute z-50 ${
+                    width < 768
+                      ? 'fixed inset-0 flex items-center justify-center'
+                      : ''
+                  }`}
+                  style={{
+                    top: `${popup.top}px`,
+                    left: `${popup.left}px`,
+                    transform: width < 768 ? 'none' : 'translate(-50%, -50%)',
+                  }}
+                >
+                  <PopupCard
+                    id={popup.content.id}
+                    title={popup.content.title}
+                    chapter={popup.content.chapter}
+                    page={popup.content.page}
+                    subtitle={popup.content.subtitle}
+                    onClose={handleClose}
+                  />
+                </div>
+              )}
+            </div>
           </div>
 
-          <div className='flex flex-row justify-center items-center'>
+          {/* Second row with map2.png and map3.png */}
+          <div className='flex justify-center items-center'>
             <div className='border-4 border-black relative-container overflow-visible m-8'>
               {width > 0 && (
                 <Image
                   src='/images/map/map2.png'
-                  alt='Cathcart Map'
+                  alt='San Francisco Chinatown and Environs'
                   width={width < 1024 ? 300 : 410}
                   height={width < 1024 ? 400 : 600}
                 />
@@ -193,20 +197,38 @@ const CathcartMap = () => {
               {width > 0 && (
                 <Image
                   src='/images/map/map3.png'
-                  alt='Cathcart Map'
+                  alt='Cathcart Map China Tower'
                   width={width < 1024 ? 300 : 250}
                   height={width < 1024 ? 400 : 400}
                 />
               )}
+
+              {/* Beach icon placed on this image */}
+              <div
+                className='icon-container'
+                style={{ top: '15%', left: '15%' }} // Adjust these values as needed
+              >
+                <Suspense fallback={<div>Loading...</div>}>
+                  <div
+                    onClick={(e) => handleIconClick('beach', e)}
+                    className='map-icon'
+                  >
+                    <FontAwesomeIcon icon={icons.beach as IconProp} />
+                  </div>
+                </Suspense>
+              </div>
             </div>
           </div>
-          <div className='pb-10'>
-            <Legend icons={icons} />
+
+          {/* Legend and Credits below the maps */}
+          <div className='w-full flex flex-col items-center'>
+            <div className='mt-8'>
+              <Legend icons={icons} />
+            </div>
+            <p className='text-md text-gray-600 mt-4 p-4'>
+              Images courtesy of Schein and Schein Inc.
+            </p>
           </div>
-          {/* Credit Section */}
-          <p className='text-md text-gray-600 mt-4 p-4'>
-            Images courtesy of Schein and Schein Inc.
-          </p>
         </div>
       </div>
     </div>
