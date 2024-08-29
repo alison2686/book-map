ad8bfa13c5ab81aa39cd1ca06da0913c677e9189
Alison Lee <alison2686@gmail.com>
2024-08-29 10:45:47 -0700

    add evolution of the book

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import Evolution from '@/components/evolution';
+import AboutCard from '@/components/about';
 
 export default function About() {
   const [width, setWidth] = useState(0);
@@ -16,41 +18,9 @@ export default function About() {
   }, []);
 
   return (
-    <div className='container mx-auto px-4'>
-      <div className='w-full h-screen flex flex-col justify-center items-center text-white bg-seafoam'>
-        <div className='flex justify-center items-center text-center w-full'>
-          <div className='grid grid-cols-2 gap-2 w-full h-full'>
-            {/* Image column */}
-            <div className='flex justify-end items-center'>
-              <Image
-                src='/images/ron-lennie.jpeg'
-                alt='Becoming American cover'
-                width={width < 1024 ? 100 : 450}
-                height={width < 1024 ? 100 : 550}
-              />
-            </div>
-            {/* Text column */}
-            <div className='flex flex-col justify-center items-center'>
-              <h1 className='text-4xl lg:text-6xl p-6'>About the Authors</h1>
-              <h4 className='max-w-xl text-2xl leading-relaxed text-left'>
-                Lennie Lee and son Ron Lee had similar careers as real estate
-                investors and public school teachers. In her spare time, she
-                wrote an extensive autobiography and memoir that included
-                detailed observations of life in San Francisco’s Chinatown
-                beyond the facade of tourism.
-              </h4>
-              <h4 className='max-w-xl text-2xl leading-relaxed text-left pt-6'>
-                Ron spent his free time advocating for a variety of issues
-                involving Asian American Native Hawaiian Pacific Islanders. At
-                Lennie’s request he was directed, along with his two daughters
-                and nephews, to edit and add to her work. The result is an
-                extraordinary adventure story and a description of the entire
-                Chinese diaspora.
-              </h4>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <AboutCard />
+      <Evolution />
+    </>
   );
 }
---
+++ src/components/evolution/index.tsx
@@ -0,0 +1,78 @@
+'use client';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+const Evolution = () => {
+  const [width, setWidth] = useState(0);
+
+  const updateWidth = () => {
+    const newWidth = window.innerWidth;
+    setWidth(newWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWidth);
+    updateWidth();
+
+    return () => {
+      window.removeEventListener('resize', updateWidth);
+    };
+  }, []);
+
+  return (
+    <div className='container mx-auto px-4 '>
+      <div className='w-full h-full flex flex-col justify-center items-center text-white bg-sky p-10'>
+        <div className='flex justify-center items-center text-center w-full'>
+          <div className='grid grid-cols-3 gap-2 w-full h-full'>
+            {/* Title spanning both columns */}
+            <div className='col-span-3'>
+              <h1 className='text-6xl lg:text-6xl p-4'>
+                Evolution of the Book
+              </h1>
+            </div>
+            {/* Image column */}
+            <div className='col-span-3 flex justify-center items-center p-4'>
+              <Image
+                src='/images/joke-sings.png'
+                alt='Joke Sings book cover'
+                width={width < 1024 ? 100 : 200}
+                height={width < 1024 ? 100 : 300}
+              />
+            </div>
+
+            {/* Text column */}
+            <div className='col-span-3 flex flex-col justify-center items-center p-4'>
+              <h4 className=' max-w-xl text-xl leading-relaxed text-left'>
+                “Uncle Ron, look at what Grandma wrote, it reads like something
+                out of “Warrior.’ He handed me a large green binder with the
+                title Joke Sings. It turned out to be a book written by LK
+                Lennie Lee (1923-2021) my mother, Erik’s grandmother. “What’s
+                Warrior?” I asked. He explained it was a television series about
+                Chinatown in the 1880s and was based on an 8 page “treatment”
+                written by Bruce Lee in the 1970s. Stories by Bruce Lee versus
+                tales by an elderly grandmother seemed unlikely. It turned out
+                Lennie’s stories were arguably more adventurous and exciting
+                than Bruce’s or those written by Warrior producers.
+              </h4>
+
+              <h4 className='row-span-2 max-w-xl text-xl leading-relaxed text-left pt-6'>
+                For example, Lennie writes that her ancestors were the
+                inspiration for one of the greatest adventure stories ever
+                written, Robert Louis Stevenson’s Treasure Island. Knowing there
+                would be doubters, our family did research to confirm
+                plausibility. Enough was collected that a book was produced. A
+                bonus is that her incredible recall allowed for detailed
+                descriptions of Chinatown life beyond the façade of tourism.
+                Realizing this work would appeal to young people, it was decided
+                to produce this version that has the look and feel of a graphic
+                novel.
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Evolution;
