8eca5cdb07cd32c0c2fc92de80bbf5eb9c5acefe
Alison Lee <alison2686@gmail.com>
2024-09-12 12:26:53 -0700

    replace logo placeholder

--- src/components/nav/navbar/index.tsx
+++ src/components/nav/navbar/index.tsx
@@ -9,26 +9,15 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
       <div className='w-full h-20 bg-peach fixed top-0 shadow-lg border-b-4 border-border  z-50'>
         <div className='container mx-auto px-4 h-full'>
           <div className='flex justify-between items-center h-full'>
-            <Logo />
-            <button
-              type='button'
-              className='inline-flex items-center md:hidden'
-              onClick={toggle}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='40'
-                height='40'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='yellow'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path d='M3 12h18M3 6h18M3 18h18' />
-              </svg>
-            </button>
+            <Link href='/'>
+              <p className='text-4xl transition duration-300 ease-in-out transform hover:scale-105'>
+                Becoming American
+                <p className=' text-xl flex justify-center'>
+                  A 600-Year Odyssey
+                </p>
+              </p>
+            </Link>
+
             <ul className='hidden md:flex gap-x-6 text-white font-comic text-lg'>
               <li>
                 <Link href='/About'>
