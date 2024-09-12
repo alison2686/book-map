05fd0eda6b2e17e7f1be84603be66e50fe8bdf67
Alison Lee <alison2686@gmail.com>
2024-09-12 12:35:48 -0700

    update nav home

--- src/components/nav/navbar/index.tsx
+++ src/components/nav/navbar/index.tsx
@@ -6,18 +6,19 @@ import Button from './Button';
 const Navbar = ({ toggle }: { toggle: () => void }) => {
   return (
     <>
-      <div className='w-full h-20 bg-peach fixed top-0 shadow-lg border-b-4 border-border  z-50'>
+      <div className='w-full h-20 bg-peach fixed top-0 shadow-lg border-b-4 border-border z-50'>
         <div className='container mx-auto px-4 h-full'>
           <div className='flex justify-between items-center h-full'>
             <Link href='/'>
-              <p className='text-4xl transition duration-300 ease-in-out transform hover:scale-105'>
-                Becoming American
-                <p className=' text-xl flex justify-center'>
+              <div className='transition duration-300 ease-in-out transform hover:scale-105'>
+                <p className='text-4xl '>Becoming American</p>
+                <p className='text-xl flex justify-center'>
                   A 600-Year Odyssey
                 </p>
-              </p>
+              </div>
             </Link>
 
+            {/* Navbar links - only visible on medium screens and larger */}
             <ul className='hidden md:flex gap-x-6 text-white font-comic text-lg'>
               <li>
                 <Link href='/About'>
@@ -41,8 +42,34 @@ const Navbar = ({ toggle }: { toggle: () => void }) => {
                 </Link>
               </li>
             </ul>
+
+            {/* Button (visible on larger screens) */}
             <div className='hidden md:block'>
               <Button />
+            </div>
+
+            {/* Hamburger menu icon for mobile view */}
+            <div className='md:hidden'>
+              <button
+                onClick={toggle}
+                className='text-white focus:outline-none'
+              >
+                {/* Hamburger icon */}
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-8 w-8 text-black'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M4 6h16M4 12h16m-7 6h7'
+                  />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
