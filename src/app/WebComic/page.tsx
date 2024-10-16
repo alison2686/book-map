582d5d7e5d731ac755ec119da756a0a25a7ccd3d
Alison Lee <alison2686@gmail.com>
2024-10-16 12:16:00 -0700

    comic cover split h2

--- src/app/WebComic/page.tsx
+++ src/app/WebComic/page.tsx
@@ -10,7 +10,7 @@ function WebComic() {
         <h1 className='comic-cover-title md:text-5xl lg:text-7xl text-center'>
           Becoming American: <br /> A 600-Year Odyssey
         </h1>
-        <h2 className='mt-4 md:mt-8 text-sm md:text-base lg:text-lg leading-relaxed text-center max-w-2xl'>
+        <h2 className='mt-4 md:mt-8 text-sm md:text-base lg:text-lg  max-w-2xl'>
           “Uncle Ron, look at what Grandma wrote, it reads like something out of
           “Warrior.’ He handed me a large green binder with the title Joke
           Sings. It turned out to be a book written by LK Lennie Lee (1923-2021)
@@ -19,9 +19,11 @@ function WebComic() {
           on an 8-page “treatment” written by Bruce Lee in the 1970s. Stories by
           Bruce Lee versus tales by an elderly grandmother seemed unlikely. It
           turned out Lennie’s stories were arguably more adventurous and
-          exciting than Bruce’s or those written by Warrior producers. For
-          example, Lennie writes that her ancestors were the inspiration for one
-          of the greatest adventure stories ever written, Robert Louis
+          exciting than Bruce’s or those written by Warrior producers.
+        </h2>
+        <h2 className='mt-4 md:mt-8 text-sm md:text-base lg:text-lg max-w-2xl'>
+          For example, Lennie writes that her ancestors were the inspiration for
+          one of the greatest adventure stories ever written, Robert Louis
           Stevenson’s Treasure Island. Knowing there would be doubters, our
           family did research to confirm plausibility. Enough was collected that
           a book was produced. A bonus is that her incredible recall allowed for
