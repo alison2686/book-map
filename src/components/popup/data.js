439415eecc915d3984bf1acaca1b5ce6c51b85d9
Alison Lee <alison2686@gmail.com>
2024-12-16 16:14:19 -0800

    add Laundries comic panel data

--- src/components/popup/data.js
+++ src/components/popup/data.js
@@ -3,7 +3,7 @@ export const PopupData = [
     id: 'littlePete',
     title: 'Little Pete Must Die',
     chapter: 'Chapter 11',
-    page: 'page 61',
+    page: 'page 63',
     subtitle:
       'The assassination of notorious San Francisco gangster Fong Ching, aka Little Pete, in a barbershop chair is one of the most infamous crime stories in Chinatown history.',
     subtitle2:
@@ -12,7 +12,7 @@ export const PopupData = [
   {
     id: 'sailboat',
     title: 'Robert Louis Stevenson V. Denis the Menace',
-    chapter: 'Chapter 9',
+    chapter: 'Chapter 8',
     page: 'page 37',
     subtitle: '“RLS” v. Denis the Menace.',
     subtitle2: '',
@@ -28,8 +28,8 @@ export const PopupData = [
   {
     id: 'beach',
     title: 'They Mystery of China Beach',
-    chapter: 'Chapter 7',
-    page: 'page 23',
+    chapter: 'Chapter 6',
+    page: 'page 24',
     subtitle:
       '“China Beach", a small, sandy cove between Lands End and Baker Beach in San Francisco’s Sea Cliff neighborhood, is used today as a public recreation area that offers spectacular views of the Marin.',
     subtitle2:
@@ -37,9 +37,9 @@ export const PopupData = [
   },
   {
     id: 'shingchong',
-    title: 'Merchants',
-    chapter: 'Chapter 25',
-    page: 'page 157',
+    title: 'The Merchant',
+    chapter: 'Chapter 28',
+    page: 'page 176',
     subtitle:
       'Being classified as a merchant was crucial during the 1882 Chinese Exclusion Act era.',
     subtitle2: '',
@@ -47,24 +47,26 @@ export const PopupData = [
   {
     id: 'cigar',
     title: 'The Cigar Makers',
-    chapter: 'Chapter 23',
-    page: 'page 136',
+    chapter: 'Chapter 25',
+    page: 'page 134',
     subtitle: 'Optimo Five Cent Cigars.',
     subtitle2: '',
   },
   {
     id: 'laundry',
     title: 'Laundries',
-    chapter: 'Chapter 24',
+    chapter: 'Chapter 26',
     page: 'page 147',
-    subtitle: 'Laundries Were a Dangerous Business.',
-    subtitle2: '',
+    subtitle:
+      'Laundries Were a Dangerous Business. A laundry business offered many opportunities to sojourners and joke-sings alike, but a huge risk also existed. ',
+    subtitle2:
+      'The business needed to be located outside Chinatown in order to be close to their clientele. This left them being the first to be exposed to nefarious characters who were all over the wild, wild town of San Francisco.',
   },
   {
     id: 'tong',
     title: 'Suey Ying Tong Memories',
     chapter: 'Chapter 14',
-    page: 'page 80',
+    page: 'page 83',
     subtitle:
       'I ventured into forbidden territory two floors above where we lived. I walked into a room where several men, smoking opium, were lying on a wooden bed made up of four saw horses laid over with a dozen wooden planks.',
     subtitle2: '',
@@ -72,8 +74,8 @@ export const PopupData = [
   {
     id: 'cameron',
     title: 'The Donaldina Cameron House',
-    chapter: 'Chapter 14',
-    page: 'page 84',
+    chapter: 'Chapter 15',
+    page: 'page 88',
     subtitle:
       'The Donaldina Cameron House, a shelter for Chinese girls who were forced into ‘white slavery ’or prostitution during the old days, was half a block away at the corner of Joice and Sacramento Street. ',
     subtitle2: '',
