31c6b2d15f0bae0917de145ffa2d7cf20ff5b33a
Alison Lee <alison2686@gmail.com>
2025-07-16 13:35:30 -0700

    google analytics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navigation from '@/components/nav';
 import Footer from '@/components/footer';
 import type { Metadata } from 'next';
 import { Inter, Bangers } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,6 +18,7 @@ export const metadata: Metadata = {
     'Based On An Autobiography and Memoir By L.K. Lennie Lee. Becoming American: A 600 Year Odyssey chronicles the interesting facts and stories found while working to confirm the plausibility of Lennie’s amazing tales',
 };
 
+const GA_MEASUREMENT_ID = 'G-Z6YHJCYGG8';
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,6 +26,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy='afterInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id='gtag-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className={`${bangers.className}`}>
         <div className='layout-grid'>
           <Navigation />
