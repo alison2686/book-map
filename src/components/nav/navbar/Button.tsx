eb79671195da3999532a35d537c2bceb0901e09f
Alison Lee <alison2686@gmail.com>
2025-09-19 10:44:26 -0700

    button hover underline

--- src/components/nav/navbar/Button.tsx
+++ src/components/nav/navbar/Button.tsx
@@ -14,7 +14,9 @@ const Button = () => {
     <div className='relative'>
       <button
         onClick={handleClick}
-        className='font-bangers h-12 rounded-lg tracking-wide bg-white font-bold px-5 hover:bg-yellow-200 transition-colors'
+        className='font-bangers h-12 rounded-lg tracking-wide bg-white font-bold px-5 hover:underline
+                         shadow-[3px_3px_0_0_#000] hover:shadow-[2px_2px_0_0_#000] transition-transform
+                         hover:translate-x-[1px] hover:translate-y-[1px]'
       >
         Purchase the Book
       </button>
