388d1f9ee5cf7318d85d838d93ba074d3c07f95c
Alison Lee <alison2686@gmail.com>
2025-12-08 15:41:01 -0800

    upgrade to latest next

--- src/components/nav/navbar/Button.tsx
+++ src/components/nav/navbar/Button.tsx
@@ -1,25 +1,19 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Button = () => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(
-      'https://shop.ingramspark.com/b/084?params=IfEF6JbbUnmtZyoTfDpbXWXVMePOCczXPGojYhjKiX5'
-    );
-  };
-
   return (
     <div className='relative'>
-      <button
-        onClick={handleClick}
-        className='font-bangers h-12 rounded-lg tracking-wide bg-white font-bold px-5 hover:underline
-                         shadow-[3px_3px_0_0_#000] hover:shadow-[2px_2px_0_0_#000] transition-transform
-                         hover:translate-x-[1px] hover:translate-y-[1px]'
+      <Link
+        href='https://shop.ingramspark.com/b/084?params=IfEF6JbbUnmtZyoTfDpbXWXVMePOCczXPGojYhjKiX5'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='inline-flex items-center justify-center font-bangers h-12 rounded-lg tracking-wide bg-white font-bold px-5 hover:underline
+                   shadow-[3px_3px_0_0_#000] hover:shadow-[2px_2px_0_0_#000] transition-transform
+                   hover:translate-x-[1px] hover:translate-y-[1px]'
       >
         Purchase the Book
-      </button>
+      </Link>
     </div>
   );
 };
